6a7af60ce1dd06ccb0f76a10884ca8372cb217da
BSKOM <b.shelestuaE@gmail.com>
2022-07-19 20:57:00 +0100

    feat:add state module

---
+++ src/assets/state.js
@@ -0,0 +1,40 @@
+const state = {
+    shop: [
+        {
+            "name": "Good1",
+            "price": "1500",
+            "ids": 1,
+            "id": 1001,
+        },
+        {
+            "name": "Good2",
+            "price": "830",
+            "ids": 1,
+            "id": 1002,
+        },
+        {
+            "name": "Good3",
+            "price": "2999",
+            "ids": 1,
+            "id": 1003,
+        },
+        {
+            "name": "Good4",
+            "price": "3999",
+            "ids": 1,
+            "id": 1004,
+        },
+
+    ],
+    cart: [
+        {
+            "id": 1001,
+            "price": "1500",
+            user: 1,
+            count: 1,
+        }
+    ]
+};
+
+export default state;
+
